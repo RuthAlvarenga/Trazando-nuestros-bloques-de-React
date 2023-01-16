293806d328dbf30af10ad1d341dbd3a492a1cfd8
Ruth Alvarenga <vanessapalmerola18@gmail.com>
2023-01-16 16:31:45 -0300

    Trazando Bloques con React

---
+++ src/Components/Advertisement.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class Advertisement extends Component {
+    render() { 
+        return <div className="advertent">
+        </div>
+    }
+}
+
+export default Advertisement;
